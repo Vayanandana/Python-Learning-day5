6c7cf1d5473419281f829e85c21e8c88a2501e93
VayanandanaBuddavarapu <vayanandanabud@gmail.com>
2026-01-05 21:24:54 +0530

    Add files via upload

---
+++ integer.py
@@ -0,0 +1,28 @@
+#All whole numbers (positive, negative and zero) without any fractional part come under Integers.
+#Examples 
+# ...-3, -2, -1, 0, 1, 2, 3,...
+
+#Example 1
+a = 10
+b = 20
+
+print(a)
+print(b)
+print(type(a))
+
+#Example 2
+x = 15
+y = 4
+
+print(x + y)   # Addition
+print(x - y)   # Subtraction
+print(x * y)   # Multiplication
+print(x // y)  # Floor Division
+print(x % y)   # Modulus
+
+#Example 3
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+print("Sum =", a + b)
+
